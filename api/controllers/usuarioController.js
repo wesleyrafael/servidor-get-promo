13ac0eab7a36281b53658cc916c686ca60a35a0a
Wesley Rafael <rafael.0404@hotmail.com>
2018-11-14 01:46:06 -0300

    Foram adicionadas funções de leitura de usuário e de remoção

--- api/controllers/usuarioController.js
+++ api/controllers/usuarioController.js
@@ -5,46 +5,146 @@ var bcrypt = require('bcrypt');
 const BCRYPT_SALT_ROUNDS = 12;
 
 exports.cadastrarUsuario = function (req, res) {
-    const data = {
-      apelido: req.body.apelido,
-      nome: req.body.nome,
-      senha: req.body.senha,
-      email: req.body.email,
-      foto: req.body.foto,
-      permissao: req.body.permissao
-    };
-    if (data.permissao === '' || data.email === '' || data.senha === '' || data.nome === '' || data.apelido == '') {
-      res.json('Dados incompletos!');
+  const data = {
+    apelido: req.body.apelido,
+    nome: req.body.nome,
+    senha: req.body.senha,
+    email: req.body.email,
+    foto: req.body.foto,
+    permissao: req.body.permissao
+  };
+  if (data.permissao === '' || data.email === '' || data.senha === '' || data.nome === '' || data.apelido == '') {
+    res.json('Dados incompletos!');
+  }
+  Usuario.findOne({
+    where: {
+      apelido: data.apelido, //adicionar funcionalidade de checar email
+    },
+  })
+  .then(usuario => {
+    if (usuario != null) {
+      console.log('apelido já existe');
+      res.json('Apelido já existe');
+    } else {
+      bcrypt
+      .hash(data.senha, BCRYPT_SALT_ROUNDS)
+      .then(function(hashedPassword) {
+        Usuario.create({
+          apelido: req.body.apelido,
+          nome: req.body.nome,
+          senha: req.body.senha, //hashedPassword
+          email: req.body.email,
+          foto: req.body.foto,
+          permissao: req.body.permissao
+        }).then(() => {
+          console.log('usuario '+ data.apelido + ' criado no db');
+          res.status(200).send({ message: 'usuario '+ data.apelido + ' criado' });
+        });
+      });
     }
-    Usuario.findOne({
-      where: {
-        apelido: data.apelido, //adicionar funcionalidade de checar email
+  })
+  .catch(err => {
+    console.log('problem communicating with db');
+    res.status(500).json(err);
+  });
+};
+
+exports.getUsuario = function (req, res) {
+  const req_apelido = req.params.apelido;
+  Usuario.findOne({
+    where: {
+      apelido: req_apelido
+    },
+  })
+  .then(usuario => {
+    if (usuario == null){
+      console.log('usuario nao existe');
+      res.json('Usuario nao existe');
+    } else {
+      res.json(usuario);
+    }
+  })
+  .catch(err => {
+    console.log('problem communicating with db');
+    res.status(500).json(err);
+  });
+};
+
+exports.getAllUsuarios = function (req, res) {
+  Usuario.findAll()
+  .then(usuarios => {
+    if (usuarios == null){
+      console.log('nenhum usuario cadastrado');
+      res.json('nenhum usuario cadastrado');
+    } else {
+      res.json(usuarios);
+    }
+  })
+  .catch(err => {
+    console.log('problem communicating with db');
+    res.status(500).json(err);
+  });
+};
+
+/*exports.mudarApelido = function (req, res) {
+  const apelido_antigo = req.params.apelidoAntigo;
+  const apelido_novo = req.body.apelido_novo;
+  if (apelido_novo == '') {
+    res.json('Dados incompletos!');
+  }
+
+  Usuario.findOne({
+    where: {
+      apelido: apelido_novo
+    },
+  })
+  .then(usuario => {
+    if (usuario =! null){
+      console.log('Apelido ja existe');
+      res.json('Apelido ja existe');
+    }
+  })
+  .catch(err => {
+    console.log('problem communicating with db');
+    res.status(500).json(err);
+  });
+
+  Usuario.findOne({
+    where: {
+        apelido: apelido_antigo
       },
     })
-      .then(usuario => {
-        if (usuario != null) {
-          console.log('apelido já existe');
-          res.json('Apelido já existe');
-        } else {
-          bcrypt
-            .hash(data.senha, BCRYPT_SALT_ROUNDS)
-            .then(function(hashedPassword) {
-              Usuario.create({
-                apelido: req.body.apelido,
-                nome: req.body.nome,
-                senha: req.body.senha, //hashedPassword
-                email: req.body.email,
-                foto: req.body.foto,
-                permissao: req.body.permissao
-              }).then(() => {
-                console.log('usuario criado no db');
-                res.status(200).send({ message: 'usuario criado' });
-              });
-            });
-        }
-      })
-      .catch(err => {
-        console.log('problem communicating with db');
-        res.status(500).json(err);
-      });
-  };
+    .then(usuario_att => {
+      usuario_att.update({apelido: apelido_novo});
+      console.log('usuario ' + apelido_antigo + ' agora é ' + apelido_novo);
+      res.json('usuario ' + apelido_antigo + ' agora é ' + apelido_novo);
+
+    })
+    .catch(err => {
+      console.log('problem communicating with db');
+      res.status(500).json(err);
+    });;
+};*/
+
+exports.deleteUsuario = function (req, res) {
+  const req_apelido = req.params.apelido;
+  Usuario.findOne({
+    where: {
+      apelido: req_apelido
+    },
+  })
+  .then(usuario => {
+    if (usuario == null){
+      console.log('usuario nao existe');
+      res.json('Usuario nao existe');
+    } else {
+      usuario.destroy();
+      console.log('usuario deletado');
+      res.json('Usuario deletado');
+    }
+  })
+  .catch(err => {
+    console.log('problem communicating with db');
+    res.status(500).json(err);
+  });
+};
--- api/routes/usuarioRoutes.js
+++ api/routes/usuarioRoutes.js
@@ -1,7 +1,21 @@
 module.exports = function(app) {
-  var anuncioController = require('../controllers/usuarioController');
+  var usuarioController = require('../controllers/usuarioController');
 
   // todoList Routes
   app.route('/usuario/cadastrarUsuario')
-    .post(anuncioController.cadastrarUsuario);
+    .post(usuarioController.cadastrarUsuario);
+
+  app.route('/usuario/getUsuario/:apelido')
+    .get(usuarioController.getUsuario);
+
+  app.route('/usuario/getAllUsuarios')
+    .get(usuarioController.getAllUsuarios);
+
+  app.route('/usuario/deleteUsuario/:apelido')
+    .delete(usuarioController.deleteUsuario);
+
+  /*app.route('/usuario/mudarApelido/:apelidoAntigo')
+    .put(usuarioController.mudarApelido);*/
+
+
 };
